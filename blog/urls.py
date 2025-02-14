48a1d8315f407c968f4cb518b1c78cbc38d09314
Antonio <antonio.jose.medina.e@gmail.com>
2025-02-13 22:34:05 -0600

    hola

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='posts'),
+    path('post/create', views.create_post, name='post-create'),
     path('about/', views.about, name='about'),
 ]
