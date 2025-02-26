034aac62581cbcf4716b28697030f59aec284252
Antonio <antonio.jose.medina.e@gmail.com>
2025-02-25 23:26:09 -0600

    yapolls

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,7 @@ from .views import (
     DetailView,
     ResultsView,
     CreateQuestionView,
+    CreateQuestionWithoutPostView,
     VoteView,
     ResetVotesView,
     EditQuestionView,
@@ -15,12 +16,11 @@ app_name = "polls"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("<int:pk>/", DetailView.as_view(), name="detail"),
-    path('new/', CreateQuestionView.as_view(), name="createnew"), 
+    path('create/<int:post_id>/', CreateQuestionView.as_view(), name='createnew'),  
+    path('create/', CreateQuestionWithoutPostView.as_view(), name='create_without_post'),
     path("<int:pk>/edit/", EditQuestionView.as_view(), name="edit"),
     path("<int:pk>/results/", ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", VoteView.as_view(), name="vote"),
     path('reset_votes/<int:question_id>/', ResetVotesView.as_view(), name='reset_votes'),
     path("delete/<int:pk>/", DeleteQuestionView.as_view(), name="delete"),
-    #path('delete/<int:question_id>/', views.delete_question, name='delete'),
-
 ]
--- polls/views.py
+++ polls/views.py
@@ -8,7 +8,7 @@ from django.db.models import Sum
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views import View
-
+from blog.models import Post
 
 class IndexView(ListView):
     template_name = "polls/index.html"
@@ -16,30 +16,6 @@ class IndexView(ListView):
 
     def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
-"""
-def index(request):
-    if request.method == 'POST':
-        question_text = request.POST.get('question_text')
-        new_question = Question.objects.create(
-            question_text=question_text,
-            pub_date=timezone.now()
-        )
-    
-        for i in range(1, 4):  
-            choice_text = request.POST.get(f'new_choice_{i}')
-            if choice_text:
-                Choice.objects.create(
-                    question=new_question,
-                    choice_text=choice_text
-                )
-
-        return redirect('polls:index') 
-    """  """
-    latest_question_list = Question.objects.all().order_by('-pub_date')
-    return render(request, 'polls/index.html', {
-        'latest_question_list': latest_question_list
-    })
-    """
 
 class DetailView(DetailView):
     model = Question
@@ -48,12 +24,6 @@ class DetailView(DetailView):
     def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.now())
 
-"""
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-"""
-
 class ResultsView(DetailView):
     model = Question
     template_name = "polls/results.html"
@@ -61,27 +31,28 @@ class ResultsView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["vote_count"] = self.object.choice_set.aggregate(Sum("votes")).get("votes__sum", 0)
-        """
-        try:
-            vote_count = self.object.choice_set.aggregate(Sum('votes'))['votes__sum']
-            context['vote_count'] = vote_count
-        except KeyError:
-            context['vote_count'] = 0
-        """
         return context
+
+
+
 
 
 class CreateQuestionView(CreateView):
     model = Question
     form_class = QuestionForm
     template_name = "polls/createnew.html"
-    #success_url = '/'
     success_url = reverse_lazy("polls:index")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post_id = self.kwargs.get('post_id')
+        context['post'] = get_object_or_404(Post, pk=post_id)
+        return context
 
     def form_valid(self, form):
         self.object = form.save()
+        post_id = self.kwargs.get('post_id')
 
-        #new_choice_text = self.request.POST.get('new_choice', '').strip()
         for i in range(1, 4):
             choice_text = self.request.POST.get(f'new_choice_{i}', '').strip()
             if choice_text:
@@ -89,14 +60,36 @@ class CreateQuestionView(CreateView):
                     question=self.object,
                     choice_text=choice_text
                 )
-        
-        return redirect('polls:createnew')
-        return super().form_valid(form)
+        return redirect(self.success_url)
+    
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['latest_question_list'] = Question.objects.all().order_by('-pub_date')
-        return context
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect(reverse('blog:post_detail', args=[post.pk]))
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
+
+class CreateQuestionWithoutPostView(CreateView):
+    model = Question
+    form_class = QuestionForm
+    template_name = "polls/createnew.html"
+    success_url = reverse_lazy("polls:index")
+
+    def form_valid(self, form):
+        self.object = form.save()
+
+        for i in range(1, 4):
+            choice_text = self.request.POST.get(f'new_choice_{i}', '').strip()
+            if choice_text:
+                Choice.objects.create(
+                    question=self.object,
+                    choice_text=choice_text
+                )
+        return redirect(self.success_url)
 
 class VoteView(View):
     def post(self, request, question_id):
